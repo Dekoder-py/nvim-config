1e96b2706a832e1b979aa2127209ad66ba12e3f5
Dekoder-py <kyleyesmariam@icloud.com>
2025-04-05 10:18:22 +1300

    seperate plugins

--- init.lua
+++ init.lua
@@ -17,31 +17,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup("plugins")
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-
-}
-
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = { "lua", "python" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
