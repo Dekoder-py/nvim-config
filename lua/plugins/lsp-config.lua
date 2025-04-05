987bba92937d0450445d4fb49ecc514c374025e2
Dekoder-py <kyleyesmariam@icloud.com>
2025-04-05 22:05:31 +1300

    add chatgpt fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,46 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "gopls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.gopls.setup({})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.diagnostic.config({
-        signs = true,
-        underline = true,
-        update_in_insert = true,
-        virtual_text = true,
-        severity_sort = true,
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "gopls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.pyright.setup({})
+			lspconfig.gopls.setup({})
+			-- Override the floating preview to limit max width
+			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
+			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+				opts = opts or {}
+				opts.border = opts.border or "rounded"
+				opts.max_width = 80 -- ✅ Set your desired max width here
+				return orig_util_open_floating_preview(contents, syntax, opts, ...)
+			end
+
+			-- Keybinding for hovering (should work as expected)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.diagnostic.config({
+				signs = true,
+				underline = true,
+				update_in_insert = true,
+				virtual_text = true,
+				severity_sort = true,
+			})
+		end,
+	},
 }
