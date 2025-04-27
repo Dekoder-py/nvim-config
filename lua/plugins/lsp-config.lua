96026d38c0a3761b84059938a738e7bfeb7b170c
Dekoder-py <kyleyesmariam@icloud.com>
2025-04-28 11:35:19 +1200

    updates to lsp configuartion for nvim 0.11.1

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "jdtls", "bashls" },
+        ensure_installed = { "lua_ls", "pyright", "ast_grep", "eslint", "html", "cssls", "bashls" },
       })
     end,
   },
@@ -19,8 +19,25 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.pyright.setup({})
-      lspconfig.jdtls.setup({})
+      lspconfig.ast_grep.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
       lspconfig.bashls.setup({})
+      vim.lsp.config('lua_ls', {})
+      vim.lsp.config('pyright', {})
+      vim.lsp.config('ast_grep', {})
+      vim.lsp.config('eslint', {})
+      vim.lsp.config('html', {})
+      vim.lsp.config('cssls', {})
+      vim.lsp.config('bashls', {})
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('pyright')
+      vim.lsp.enable('ast_grep')
+      vim.lsp.enable('eslint')
+      vim.lsp.enable('html')
+      vim.lsp.enable('cssls')
+      vim.lsp.enable('bashls')
       require("hover-fix")
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
