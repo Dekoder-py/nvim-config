bbd4c07dc0c60317dfa7bece8eced36873423544
Dekoder-py <kyleyesmariam@icloud.com>
2025-05-07 09:17:03 +1200

    changes

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   config = function ()
     vim.keymap.set('n', '<leader>u', ':TransparentToggle<CR>', {})
-    require('transparent').clear_prefix('NeoTree')
+--    require('transparent').clear_prefix('NeoTree')
 --    require('transparent').clear_prefix('lualine')
   end
 }
