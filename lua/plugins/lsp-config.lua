17365dcbc936fd705d3d77d83046c7c18c376ded
Dekoder-py <dekod3r.py@gmail.com>
2025-07-14 16:28:24 +1200

    remove web dev

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "ast_grep", "eslint", "html", "cssls", "bashls" },
+        ensure_installed = { "lua_ls", "pyright",  "bashls" },
       })
     end,
   },
@@ -26,17 +26,9 @@ return {
       lspconfig.bashls.setup({})
       vim.lsp.config('lua_ls', {})
       vim.lsp.config('pyright', {})
-      vim.lsp.config('ast_grep', {})
-      vim.lsp.config('eslint', {})
-      vim.lsp.config('html', {})
-      vim.lsp.config('cssls', {})
       vim.lsp.config('bashls', {})
       vim.lsp.enable('lua_ls')
       vim.lsp.enable('pyright')
-      vim.lsp.enable('ast_grep')
-      vim.lsp.enable('eslint')
-      vim.lsp.enable('html')
-      vim.lsp.enable('cssls')
       vim.lsp.enable('bashls')
       require("hover-fix")
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
