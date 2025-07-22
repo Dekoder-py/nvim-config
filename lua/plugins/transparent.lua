f28b92d3b2711e25629bc198e990d0d60e7c0d36
Dekoder-py <dekod3r.py@gmail.com>
2025-07-22 19:41:56 +1200

    remove a lot

--- lua/plugins/transparent.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'xiyaowong/transparent.nvim',
-  priority = 1000,
-  config = function ()
-    vim.keymap.set('n', '<leader>u', ':TransparentToggle<CR>', {})
-    require('transparent').clear_prefix('NeoTree')
---    require('transparent').clear_prefix('lualine')
-  end
-}
