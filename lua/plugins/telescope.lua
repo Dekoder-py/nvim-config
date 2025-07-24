14dfdd0ab2af6f8c7e50b87605c123d416b63c0a
Dekoder-py <dekod3r.py@gmail.com>
2025-07-25 10:24:28 +1200

    change tele keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fs', function()
