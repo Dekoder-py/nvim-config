28a3a0917b2d55460a3532ad813f395da3a1c7c6
Dekoder-py <dekod3r.py@gmail.com>
2025-07-27 13:30:55 +1200

    oops

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "bashls", "cpptools", "jdtls", "ts_ls" },
+        ensure_installed = { "lua_ls", "pyright", "bashls", "gopls", "jdtls", "ts_ls" },
       })
     end,
   },
