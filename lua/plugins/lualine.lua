a0687aa713159f61817aa007e79edcb9a1e2a19e
Dekoder-py <dekod3r.py@gmail.com>
2025-11-14 10:18:56 +1300

    tpipeline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,12 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup()
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup()
+		if os.getenv("TMUX") then
+			vim.defer_fn(function()
+				vim.o.laststatus = 0
+			end, 0)
+		end
+	end,
 }
