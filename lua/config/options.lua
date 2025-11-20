6562e3782aa3c44af19f3e26fa82a488634d1441
Dekoder-py <dekod3r.py@gmail.com>
2025-11-21 09:09:18 +1300

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -62,6 +62,9 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
+-- Explorer
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>")
+
 -- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
