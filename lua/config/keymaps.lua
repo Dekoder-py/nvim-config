4e13e9f52e40e423d79f4a4f1a341392afb7d14e
Dekoder-py <dekod3r.py@gmail.com>
2026-02-11 11:01:04 +1300

    start rework

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- tmux-sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- yank to sys. clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>p", '"+p')
+
+-- search keeps result centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- center bottom line
+vim.keymap.set("n", "G", "Gzz")
+
+-- clear highlight
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,3 @@
--- leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- tmux-sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
 -- tab settings
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
@@ -20,19 +13,6 @@ vim.o.termguicolors = true
 
 -- keep lines on screen
 vim.o.scrolloff = 10
-
--- yank to sys. clipboard
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
-vim.keymap.set("n", "<leader>p", '"+p')
-
--- search keeps result centered
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- center bottom line
-vim.keymap.set("n", "G", "Gzz")
 
 -- line numbers and sign column
 vim.opt.number = true
@@ -52,15 +32,6 @@ vim.opt.smartcase = true
 -- split prefs
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
--- clear highlight
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- VIM
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
