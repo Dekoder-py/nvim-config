7b4373391e437c7d7a1e463e01651fbd9d3f8c48
Dekoder-py <kyle@codingcorner.dev>
2026-02-17 20:31:41 +1300

    remove tmux pipeline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,10 +3,5 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup()
-		if os.getenv("TMUX") then
-			vim.defer_fn(function()
-				vim.o.laststatus = 0
-			end, 0)
-		end
 	end,
 }
