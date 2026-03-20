77cbb5e10d7b8734fee05e839e896c53aea639cb
Dekoder-py <kyle@codingcorner.dev>
2026-03-21 07:32:28 +1300

    install and configure treesitter

--- init.lua
+++ init.lua
@@ -28,6 +28,9 @@ vim.opt.splitright = true -- split to the right
 vim.opt.splitbelow = true -- split down
 vim.opt.fillchars = { eob = " " } -- hide "~" on empty lines
 vim.opt.wrap = false -- turn off wrapping
+vim.opt.foldmethod = "expr" -- use expression for folding
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- use treesitter for folding
+vim.opt.foldlevel = 99 -- start with all folds open
 vim.opt.swapfile = false -- turn off swapfile
 local undodir = vim.fn.expand("~/.vim/undodir")
 if
@@ -266,6 +269,7 @@ vim.pack.add({
   "https://github.com/rcarriga/nvim-notify",
   "https://github.com/nvim-lua/plenary.nvim",
   "https://github.com/NeogitOrg/neogit",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
 })
 
 packadd("vim-wakatime")
@@ -273,10 +277,61 @@ packadd("fzf-lua")
 packadd("nvim-notify")
 packadd("plenary.nvim")
 packadd("neogit")
+packadd("nvim-treesitter")
 
 -- ================================
 -- PLUGINS (config)
 -- ================================
+
+-- Treesitter
+local setup_treesitter = function()
+  local treesitter = require("nvim-treesitter") 
+  treesitter.setup({})
+  local ensure_installed = {
+    "vim",
+    "vimdoc",
+    "rust",
+    "c",
+    "cpp",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "astro",
+    "svelte",
+    "markdown",
+    "bash",
+    "python"
+  }
+
+  local config = require("nvim-treesitter.config")
+
+  local already_installed = config.get_installed()
+  local parsers_to_install = {}
+
+  for _, parser in ipairs(ensure_installed) do
+    if not vim.tbl_contains(already_installed, parser) then
+      table.insert(parsers_to_install, parser)
+    end
+  end
+
+  if #parsers_to_install > 0 then
+		treesitter.install(parsers_to_install)
+	end
+
+  local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		callback = function(args)
+			if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+				vim.treesitter.start(args.buf)
+			end
+    end,
+	})
+end
+
+setup_treesitter()
 
 -- FZF
 require("fzf-lua").setup({})
